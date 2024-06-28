fc0078046c7e7d3158d99efedbf9933e29e360fa
ZiaKfa <zia14148@gmail.com>
2024-06-28 11:36:58 +0700

    menyelesaikan tabel payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 
@@ -18,7 +19,10 @@ class PaymentController extends Controller
 
     public function create()
     {
-        return view('payment.create');
+        return view('payment.create',[
+            'title' => 'Create Payment',
+            'orders' => Order::all()
+        ]);
     }
 
     public function store(Request $request)
@@ -42,6 +46,31 @@ class PaymentController extends Controller
             'payment' => $payment,
             'title' => 'Payment Details'
         ]);
+    }
+
+    public function edit(Payment $payment)
+    {
+        $orders = Order::all();
+        return view('payment.edit', [
+            'payment' => $payment,
+            'title' => 'Edit Payment',
+            'orders' => $orders
+        ]);
+    }
+
+    public function update(Request $request, Payment $payment)
+    {
+        $request->validate([
+            'order_id' => 'required',
+            'payment_number' => 'required',
+            'amount' => 'required',
+        ]);
+        $payment->update([
+            'order_id' => $request->order_id,
+            'payment_number' => $request->payment_number,
+            'amount' => $request->amount,
+        ]);
+        return redirect()->route('payment.index')->with('status', 'Payment updated successfully');
     }
 
 }
--- resources/views/payment/create.blade.php
+++ resources/views/payment/create.blade.php
@@ -1,35 +1,35 @@
 <x-navbar>
-    Create Order
+    Create Payment
 </x-navbar>
 
 <x-header>
-    Create New Order
+    Create New Payment
 </x-header>
 
 <x-main>
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="{{route('order.store')}}" method="POST" enctype="multipart/form-data">
+    <form class="space-y-6" action="{{route('payment.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('POST')
       <div>
-        <label for="invoice" class="block text-sm font-medium leading-6 text-gray-900">Invoice</label>
+        <label for="payment_number" class="block text-sm font-medium leading-6 text-gray-900">Payment Number</label>
         <div class="mt-2">
-          <input id="invoice" name="invoice" type="text" autocomplete="invoice" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input id="payment_number" name="payment_number" type="text" autocomplete="payment_number" required class="block w-full rounded-md bpayment-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
       </div>
       <div>
-        <label for="quantity" class="block text-sm font-medium leading-6 text-gray-900">Quantity</label>
+        <label for="amount" class="block text-sm font-medium leading-6 text-gray-900">Amount</label>
         <div class="mt-2">
-          <input id="quantity" name="quantity" type="number" autocomplete="quantity" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input id="amount" name="amount" type="number" autocomplete="amount" required class="block w-full rounded-md bpayment-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
       </div>
       <div>
-        <label for="product_id" class="block text-sm font-medium leading-6 text-gray-900">Product</label>
+        <label for="order_id" class="block text-sm font-medium leading-6 text-gray-900">Order</label>
         <div class="mt-2">
-          <select id="product_id" name="product_id" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-            @foreach($products as $product)
-            <option value="{{$product->id}}">{{$product->name}}</option>
+          <select id="order_id" name="order_id" class="block w-full rounded-md bpayment-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            @foreach($orders as $order)
+            <option value="{{$order->id}}">{{$order->invoice}}</option>
             @endforeach
           </select>
         </div>
